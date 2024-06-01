1b68592d5a801b00325ce169c21c6d46fb52b765
pablo_renato22ati Carvalho <pab.rena15@gmail.com>
2024-06-01 02:48:45 +0000

    Adicionada a função de validar a resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,7 @@ let pontos = 0
 let pergunta = 8
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -106,8 +107,18 @@ function guardarResposta(evento) {
     resposta = evento.target.value
     idInputResposta = evento.target.id
 
-    console.log(resposta)
-    console.log(idInputResposta)
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+    if (resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos+1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id","correta")
+    }
 }
 
 async function iniciar() {
@@ -118,6 +129,10 @@ async function iniciar() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaId = input.id
+        }
     })
 }
 
